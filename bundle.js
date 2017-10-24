8dcf43a34f50bf7d2bc0d9d761c40eab78499fcf
kozenumezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-10-24 23:37:45 +0900

    Updates

--- bundle.js
+++ bundle.js
@@ -70,7 +70,27 @@
 "use strict";
 
 
-console.log('test');
+fetch('u0.json').then(function (response) {
+    return response.json();
+}).then(function (json) {
+    draw(json);
+});
+console.log('a');
+function draw(vecArray) {
+    var ctx = document.getElementById('app').getContext('2d');
+    for (var i = 0; i < 300; i++) {
+        for (var j = 0; j < 300; j++) {
+            console.log(vecArray[i][j]);
+            var color = getColor(vecArray[i][j]);
+        }
+    }
+}
+
+function getColor(num) {
+    var low = 300;
+    var range = 400;
+    console.log(num);
+}
 
 /***/ })
 /******/ ]);
