0eea52c74aa79a42e9273661507b76d71090ab49
maywanting <maywanting@gmail.com>
2017-10-24 21:30:10 +0900

    add git pages

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,21 @@
 fetch('u0.json')
     .then(response => response.json())
     .then((json) => {
-        console.log('aaaa');
-        console.log(json);
+        draw(json);
   });
+
+function draw(vecArray) {
+    var ctx = document.getElementById('app').getContext('2d');
+    for (var i = 0; i < 300; i++) {
+        for (var j = 0; j < 300; j++) {
+            console.log(vecArray[i][j]);
+            var color = getColor(vecArray[i][j]);
+        }
+    }
+}
+
+function getColor(num) {
+    var low = 300;
+    var range = 400;
+    console.log(num);
+}
