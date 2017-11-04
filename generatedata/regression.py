97887ca4bff3c04c4a2d45a48e1ef63c27bfe5af
Renne <renne00.xv@gmail.com>
2017-11-04 14:19:39 +0900

    update : regression.py

--- generatedata/regression.py
+++ generatedata/regression.py
@@ -11,13 +11,23 @@ with open(filePath, 'r') as readfile:
     data_json = json.load(readfile)
 scalar_fields = np.array(data_json, np.float32)
 
+dt = 0.0006250000000000001*10 #sec
+dx = 0.1 #m
+dy = 0.1 #m
 #配列サイズ 51*100*100
-f_t = np.gradient(scalar_fields, axis=0)
-f_x = np.gradient(scalar_fields, axis=1)
-f_y = np.gradient(scalar_fields, axis=2)
-f_xx = np.gradient(f_x, axis=1)
-f_yy = np.gradient(f_y, axis=2)
+f_t = np.gradient(scalar_fields, axis=0)/dt
+f_x = np.gradient(scalar_fields, axis=1)/dx
+f_y = np.gradient(scalar_fields, axis=2)/dy
+f_xx = np.gradient(f_x, axis=1)/dx
+f_yy = np.gradient(f_y, axis=2)/dy
 
+#配列サイズチェック
+# print('scalar_fields',scalar_fields.shape)
+# print('f_t',f_t.shape)
+# print('f_x',f_x.shape)
+# print('f_y',f_y.shape)
+# print('f_xx',f_xx.shape)
+# print('f_yy',f_yy.shape)
 
 ###gradientチェック用グラフ###
 # plt.figure()
@@ -40,54 +50,48 @@ f_yy = np.gradient(f_y, axis=2)
 # plt.draw()
 # plt.show()
 
-# scalar_fields = scalar_fields - scalar_fields[0,:,:]
-# print(scalar_fields[0,:,:])
-# print(scalar_fields[50,:,:])
-
 #回帰分析できる形に変形
-f = scalar_fields.flatten()
+# f = scalar_fields.flatten()
+# f_t_col = f_t.flatten()
+# f_x_col = f_x.flatten()
+# f_y_col = f_y.flatten()
+# f_xx_col = f_xx.flatten()
+# f_yy_col = f_yy.flatten()
+# partial = np.c_[f_xx_col, f_yy_col, f, f_x_col, f_y_col]
+
+#回帰分析変数を最低限に
 f_t_col = f_t.flatten()
-f_x_col = f_x.flatten()
-f_y_col = f_y.flatten()
 f_xx_col = f_xx.flatten()
 f_yy_col = f_yy.flatten()
-partial = np.c_[f_t_col, f_x_col, f_y_col, f_xx_col, f_yy_col]
+partial = np.c_[f_xx_col, f_yy_col]
 
-###ある座標(x,y)でならちゃんと出そう？，でもそれだとサンプル少ない
-###多数の点で回帰分析をして係数の平均を取るとか？
-# f = scalar_fields[:,49,49].flatten()
-# f_t_col = f_t[:,49,49].flatten()
-# f_x_col = f_x[:,49,49].flatten()
-# f_y_col = f_y[:,49,49].flatten()
-# partial = np.c_[f_t_col, f_x_col, f_y_col]
-
-###fを無くしてみる，ダメっぽい，むしろ微分項を増やすべき？###
-# f = f_t_col
-# partial = np.c_[f_x_col, f_y_col]
+###一部の範囲で回帰分析してみるとD=4でちゃんと結果がでる
+# f_t_col = f_t[:,40:60,40:60].flatten()
+# f_xx_col = f_xx[:,40:60,40:60].flatten()
+# f_yy_col = f_yy[:,40:60,40:60].flatten()
+# partial = np.c_[f_xx_col, f_yy_col]
 
 #配列サイズ確認
-# print('f',f.shape)
-# print('f_t_col',f_t_col.shape)
-# print('f_x_col',f_x_col.shape)
-# print('f_y_col',f_y_col.shape)
-# print('partial',partial.shape)
+#print('f',f.shape)
+print('f_t_col',f_t_col.shape)
+#print('f_x_col',f_x_col.shape)
+#print('f_y_col',f_y_col.shape)
+print('f_xx_col',f_xx_col.shape)
+print('f_yy_col',f_yy_col.shape)
+print('partial',partial.shape)
 
 #予測モデル
-reg.fit(partial,f)
+reg.fit(partial,f_t_col)
 
 #回帰係数
-print(pd.DataFrame({"Name":['f_t', 'f_x', 'f_y', 'f_xx', 'f_yy'],
+print(pd.DataFrame({"Name":['f_xx', 'f_yy'],#, 'f', 'f_x', 'f_y'],
                     "Coefficients":reg.coef_}).sort_values(by='Coefficients'))
 print(reg.intercept_)
+print(reg.score(partial, f_t_col))
 
-###Q.なぜちゃんと結果が出ないのか？
-###A.座標が2次元のせいで初期値が2次元で存在してるのがうまくいかない理由？
+###  D=4.になるはず
+###Q.なぜ結果が少しずれるのか？
+###     データが少ない？
+###     座標が2次元のせいで初期値が2次元で存在してるのがうまくいかない理由？
+###     初期値700の範囲に対してのみ回帰分析を行なった結果D=4が求められた
 ###     それなら係数自体はどの座標点でとっても一定になるはず？
-###  数値変化がちゃんと起きている点に対してのみ回帰分析をかけるべきかもしれない
-###     変化なしはノイズデータなのか？邪魔にはなっていないのか？
-###     そもそも変化なしの点が多くサンプルが足りていない可能性
-###  方程式自体が不十分なのもありうる
-###     微分項が足りてない可能性もある
-###     xとyの２階微分を追加したらちょっとよくなった？
-###Q.エクセルで一次元とはいえ複数点の場所を回帰分析してちゃんと結果が出ていたのはなぜか
-###A.微分項しかないから定数部分が消去されて綺麗に見えていたとか？
