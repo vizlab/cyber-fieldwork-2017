6c3251d0df4cb78f4096d1ff933d1ce234e3bbbd
kozenumezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-11-04 13:42:45 +0900

    Add: to generate difference data for ccm

--- ccm/ccm-gradient.R
+++ ccm/ccm-gradient.R
@@ -2,50 +2,47 @@ library(rEDM)
 library(jsonlite)
 library(multispatialCCM)
 
-gradient_fields <- fromJSON("../front/public/gradient-fields.json")
-gradient_fields <- gradient_fields$data
-N_TIMESTEP <- dim(gradient_fields)[1]
+data <- fromJSON("./difference-data.json")
+f <- data$f
+f_t <- data$f_t
+f_x <- data$f_x
+f_y <- data$f_y
+f_tt <- data$f_tt
+f_xx <- data$f_xx
+f_yy <- data$f_yy
 
-a_x <- 50
-a_y <- 50
-b_x <- 30
-b_y <- 30
+#gradient_fields <- fromJSON("../front/public/gradient-fields.json")
+#dx_max <- gradient_fields$x_grad_max
+#dy_max <- gradient_fields$y_grad_max
+#gradient_fields <- gradient_fields$data
+#axis_max <- ifelse(dx_max > dy_max, dx_max, dy_max)
 
-dx_time_series <- c()
-dy_time_series <- c()
+N_TIMESTEP <- dim(f)[1]
+
+a_x <- 10
+a_y <- 25
+
+a_time_series <- c()
+b_time_series <- c()
 for (t in 1:N_TIMESTEP) {
-  x_grad <- gradient_fields[t,]$x[[1]]
-  y_grad <- gradient_fields[t,]$y[[1]]
-  dx_time_series <- c(dx_time_series, x_grad[a_x, a_y])
-  dy_time_series <- c(dy_time_series, y_grad[a_x, a_y])
+  a_time_series <- c(a_time_series, f_yy[t, a_x, a_y])
+  b_time_series <- c(b_time_series, f_xx[t, a_x, a_y])
 }
-plot(dx_time_series)
-plot(dy_time_series)
-
-a_max <- max(scalar_fields[, a_x, a_y])
-b_max <- max(scalar_fields[, b_x, b_y])
-axis_max <- ifelse(a_max > b_max, a_max, b_max)
-
 
 # show data
-plot(a_time_series, type="l", col=1, lwd=2, xlim=c(0, N_TIMESTEP), ylim=c(0, axis_max), xlab="time step", ylab="Normalized Value", cex.lab = 1.5)
-lines(b_time_series, type="l", col=2, lty=2, lwd=2, cex.lab = 1.5)
+plot(a_time_series, type="l", col=1, lwd=2)
+lines(b_time_series, type="l", col=2, lty=2, lwd=2)
 legend("bottomright", c("a", "b"), cex=1.5, lty=c(1,2), col=c(1,2), lwd=2, bty="n")
 
-
 # determine Embedding Dimension
-lib <- c(1, 20)
-pred <- c(21, 50)  
+lib <- c(1, 30)
+pred <- c(31, 50)  
 simplex_output <- simplex(a_time_series, lib, pred)
 plot(simplex_output$E, simplex_output$rho, type = "l", xlab = "Embedding Dimension (E)", ylab = "Forecast Skill (rho)")
-E_a = 2
 
-
-lib <- c(1, 20)
-pred <- c(21, 50)  
 simplex_output <- simplex(b_time_series, lib, pred)
 plot(simplex_output$E, simplex_output$rho, type = "l", xlab = "Embedding Dimension (E)", ylab = "Forecast Skill (rho)")
-E_b = 2
+E_a <- 2
 
 # create trajectory vector for attractor
 E <- 2
@@ -59,17 +56,17 @@ for (t in 1 : X_N) {
     x[t,j] <- a_time_series[(t + BACK_MAX) - (j - 1) * TAU]
   }
 }
-plot(x, xlim=c(0, 700), ylim=c(0,700), xlab = "x(t)", ylab = "x(t-1)")
+plot(x, xlab = "x(t)", ylab = "x(t-1)")
 
 # Prediction Decay
 simplex_output <- simplex(a_time_series, lib, pred, E = E_a, tp = 1:10)
 par(mar = c(4, 4, 1, 1))
 plot(simplex_output$tp, simplex_output$rho, type = "l", xlab = "Time to Prediction (tp)", ylab = "Forecast Skill (rho)")
 
-simplex_output <- simplex(b_time_series, lib, pred, E = E_b, tp = 1:10)
+simplex_output <- simplex(b_time_series, lib, pred, E = E_a, tp = 1:10)
 par(mar = c(4, 4, 1, 1))
 plot(simplex_output$tp, simplex_output$rho, type = "l", xlab = "Time to Prediction (tp)", ylab = "Forecast Skill (rho)")
-TAU = 1
+TAU = 5
 
 # Identifying Nonlinearity
 smap_output <- s_map(a_time_series, lib, pred, E = E_a)
@@ -81,5 +78,15 @@ par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
 plot(smap_output$theta, smap_output$rho, type = "l", xlab = "Nonlinearity (theta)", ylab = "Forecast Skill (rho)")
 
 # CCM
+a_b_dataframe <- data.frame(a=a_time_series, b=b_time_series)
+a_xmap_b <- ccm(a_b_dataframe, E = E_a, lib_column = "a", target_column = "b", lib_sizes = seq(10, N_TIMESTEP, by = 5), random_libs = FALSE)
+b_xmap_a <- ccm(a_b_dataframe, E = E_a, lib_column = "b", target_column = "a", lib_sizes = seq(10, N_TIMESTEP, by = 5), random_libs = FALSE)
 
+a_xmap_b_means <- ccm_means(a_xmap_b)
+b_xmap_a_means <- ccm_means(b_xmap_a)
+
+par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
+plot(a_xmap_b_means$lib_size, pmax(0, a_xmap_b_means$rho), type = "l", col = "red", xlab = "Library Size", ylab = "Cross Map Skill (rho)", ylim = c(0, 1))
+lines(b_xmap_a_means$lib_size, pmax(0, b_xmap_a_means$rho), col = "blue")
+legend(x = "topleft", legend = c("a xmap b", "b xmap a"), col = c("red", "blue"), lwd = 1, inset = 0.02, cex = 0.8)
 
